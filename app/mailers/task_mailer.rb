e1ff434d400a66d31086bd02d40e75df1cc56828
bhanuteja6 <bhanuteja6@gmail.com>
2012-10-26 12:54:18 -0400

    my changes

--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -1,30 +1,32 @@
 class TaskMailer < ActionMailer::Base
   default from: "redmine.logicmatter@gmail.com"
-def task_creation(usermail,iname,tid)
-@user = usermail
-@user_id = iname
-@task_id = tid
-@task = Task.find(:all , :conditions => {:id => @task_name })
 
-    mail(:to =>@user, :subject => "Task", :from => "redmine.logicmatter@gmail.com")
-  end
+	def task_creation(usermail,iname,tid)
+		@user = usermail
+		@user_id = iname
+		@task_id = tid
+		@task = Task.find(:all , :conditions => {:id => @task_name })
 
-def task_update(usermail,tid,week)
-@task_acceptor = usermail
-@task = tid
-@week = week
-@task = Task.find(:all , :conditions => {:id => @task })
-@tasks = Task.find(:all)
+		mail(:to =>@user, :subject => "Task", :from => "redmine.logicmatter@gmail.com")
+	  end
 
-    mail(:to =>@task_acceptor.mail, :subject => "Task", :from => "redmine.logicmatter@gmail.com")
-  end  
-def weekly_mail(user)
-@user = user
-@user_mail = User.find(@user)
-@today = Date.today
-@tasks = Task.find(:all, :conditions => {:acceptor => @user_mail.lastname })
+	def task_update(usermail,tid,week)
+		@task_acceptor = usermail
+		@task = tid
+		@week = week
+		@task = Task.find(:all , :conditions => {:id => @task })
+		@tasks = Task.find(:all)
 
-mail(:to =>@user_mail.mail, :subject => "Task", :from => "redmine.logicmatter@gmail.com")
-end
+		mail(:to =>@task_acceptor.mail, :subject => "Task", :from => "redmine.logicmatter@gmail.com")
+	  end  
+	def weekly_mail(user)
+		@user = user
+		@user_mail = User.find(@user)
+		@today = Date.today
+		@tasks = Task.find(:all, :conditions => {:acceptor => @user_mail.lastname })
+
+		mail(:to =>@user_mail.mail, :subject => "Task", :from => "redmine.logicmatter@gmail.com")
+	end
+
 
 end
